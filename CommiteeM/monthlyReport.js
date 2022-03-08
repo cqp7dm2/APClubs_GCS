edeff398d3ba1047c5ce9025d94e575699fcc965
RyanTheCanadian <ryanyap555@gmail.com>
2022-03-09 06:25:46 +0800

    Minor changes
    Minor changes for reporting and nav fixing

--- CommiteeM/monthlyReport.js
+++ CommiteeM/monthlyReport.js
@@ -7,73 +7,88 @@ var firebaseConfig = {
     messagingSenderId: "711066755172",
     appId: "1:711066755172:web:8187fe865e562d32807f71",
     measurementId: "G-NCK9V02TE8"
-  };
-  
+};
+
 firebase.initializeApp(firebaseConfig);
 
 // Database references
 var database = firebase.database();
 
 // Getting club info
-function SelectClubData(){
-    var currentUID = firebase.auth().currentUser.uid;
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        var uids = user.uid;
+        console.log(uids);
+        firebase.database().ref('/committee/' + uids).once('value',
+            function (snapshot) {
+                snapshot.forEach(function (childSnapshot){
+                    var club = childSnapshot.val();
+                    console.log(club);
+                    SelectAllData(club);
+                });
+            });
 
-    firebase.database().ref('/committee/' + currentUID).once('value',
-    function(AllRecords){
-        AllRecords.forEach(
-            function(CurrentRecord){
-                var club = CurrentRecord.val().Committee;
-            }
-        );
-    });
-}
+    }
+});
+// function SelectClubData() {
+//     var currentUID = firebase.auth().currentUser.uid;
+
+//     firebase.database().ref('/committee/' + currentUID).once('value',
+//         function (AllRecords) {
+//             AllRecords.forEach(
+//                 function (CurrentRecord) {
+//                     var club = CurrentRecord.val().Committee;
+//                 }
+//             );
+//         });
+// }
 
 // Getting the data
-function SelectAllData(){
+function SelectAllData(club) {
+    console.log(club);
     document.getElementById("tbody1").innerHTML = "";
     event_number = 0;
-    firebase.database().ref('/clubMember/badminton').once('value',
-    function(AllRecords){
-        AllRecords.forEach(
-            function(CurrentRecord){
-                var fullName = CurrentRecord.val().FullName;
-                var tpNumber = CurrentRecord.val().TPNumber;
-                var intakeCode = CurrentRecord.val().IntakeCode;
-                var email = CurrentRecord.val().Email;
-                AddItemsToTable(fullName,tpNumber,intakeCode,email);
-            }
-        );
-    });
-}
 
-window.onload = SelectAllData;
+    firebase.database().ref('/clubMember/' + club).once('value',
+        function (AllRecords) {
+            AllRecords.forEach(
+                function (CurrentRecord) {
+                    var fullName = CurrentRecord.val().FullName;
+                    var tpNumber = CurrentRecord.val().TPNumber;
+                    var intakeCode = CurrentRecord.val().IntakeCode;
+                    var email = CurrentRecord.val().Email;
+                    AddItemsToTable(fullName, tpNumber, intakeCode, email);
+                }
+            );
+        });
+}
 
 // Inserting data to the table
 var cns_list = [];
 
-                function AddItemsToTable(fullName,tpNumber,intakeCode,email){
-                    var tbody1 = document.getElementById('tbody1');
-                    var trow = document.createElement('tr');
-                    var td1 = document.createElement('td');
-                    var td2 = document.createElement('td');
-                    var td3 = document.createElement('td');
-                    var td4 = document.createElement('td');
-                    var td5 = document.createElement('td');
+function AddItemsToTable(fullName, tpNumber, intakeCode, email) {
+    var tbody1 = document.getElementById('tbody1');
+    var trow = document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    var td4 = document.createElement('td');
+    var td5 = document.createElement('td');
 
-                    cns_list.push([fullName,tpNumber,intakeCode,email]);
-                    console.log("Im here");
-                    td1.innerHTML = ++event_number;
-                    td2.innerHTML = fullName;
-										td2.classList += "Namefield";
-                    td3.innerHTML = tpNumber;
-                    td4.innerHTML = intakeCode;
-                    td5.innerHTML = email;
+    cns_list.push([fullName, tpNumber, intakeCode, email]);
 
-                    trow.appendChild(td1);
-                    trow.appendChild(td2);
-                    trow.appendChild(td3);
-                    trow.appendChild(td4);
-                    trow.appendChild(td5);
+    td1.innerHTML = ++event_number;
+    td2.innerHTML = fullName;
+    td2.classList += "Namefield";
+    td3.innerHTML = tpNumber;
+    td4.innerHTML = intakeCode;
+    td5.innerHTML = email;
 
-                    tbody1.appendChild(trow);
-                }
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+
+    tbody1.appendChild(trow);
+}
